ef1f8eaf10da5b3d5954d110b322c95e53865086
Leonard Mueller <leonard.mueller@sap.com>
2025-12-09 18:36:14 +0100

    Initial project commit

---
+++ src/main/kotlin/de/gutselcraft/gutselCraft/GutselCraft.kt
@@ -0,0 +1,14 @@
+package de.gutselcraft.gutselCraft
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class GutselCraft : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
