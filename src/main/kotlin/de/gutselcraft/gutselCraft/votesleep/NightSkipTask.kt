782240ec65fc2f063a116070d8db134c027f465c
Syntaxx <syntaxhdyt@gmail.com>
2025-12-15 17:34:52 +0100

    Fix to slow animation

--- src/main/kotlin/de/gutselcraft/gutselCraft/votesleep/NightSkipTask.kt
+++ src/main/kotlin/de/gutselcraft/gutselCraft/votesleep/NightSkipTask.kt
@@ -16,7 +16,7 @@ class NightSkipTask : BukkitRunnable() {
         val currentTime = world.time
         
         if (currentTime >= 12541) {
-            world.time = currentTime + 10
+            world.time = currentTime + 50
         } else {
             // Clear weather
             if (world.hasStorm()) {
